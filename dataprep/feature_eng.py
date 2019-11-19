2abd9127d1a9dc4b0623cef06989f063a6de01c2
Thomas Wang <250395@live.fr>
2019-11-19 15:50:59 +0100

    Added bin_age function

--- dataprep/feature_eng.py
+++ dataprep/feature_eng.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def bin_age(df):
-    """Returns df with new column 'age_bin'"""
+    """Returns df with new column 'age_bin' and without column 'age'"""
     bin_names = []
     for i in range(2, 8):
         bin_names.append(str(i) + '0s')
@@ -10,6 +10,15 @@ def bin_age(df):
 
     df['age_bin'] = df['age_bin'].cat.add_categories('60+')
     df.loc[(df['age_bin'] == '60s') | (df['age_bin'] == '70s'), 'age_bin'] = '60+'
-    df['age_bin'] = X['age_bin'].cat.remove_unused_categories()
+    df['age_bin'] = df['age_bin'].cat.remove_unused_categories()
     df = df.drop(columns='age')
     return df
+
+
+def gender_marriage(df):
+    """Returns df with a cross-categorical column gender x marriage"""
+
+    
+
+
+    return None
