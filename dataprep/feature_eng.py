4484922fde2557f4728a0c078f66213efc082ccb
Thomas Wang <250395@live.fr>
2019-11-19 15:45:18 +0100

    Added a feature_eng module

---
+++ dataprep/feature_eng.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def bin_age(df):
+    """Returns df with new column 'age_bin'"""
+    bin_names = []
+    for i in range(2, 8):
+        bin_names.append(str(i) + '0s')
+    df['age_bin'] = pd.cut(df['age'], 6, labels=bin_names)
+
+    df['age_bin'] = df['age_bin'].cat.add_categories('60+')
+    df.loc[(df['age_bin'] == '60s') | (df['age_bin'] == '70s'), 'age_bin'] = '60+'
+    df['age_bin'] = X['age_bin'].cat.remove_unused_categories()
+    df = df.drop(columns='age')
+    return df
