1b14396eb99dd8e335cc7ebca0cf2c6af0b034b8
Thomas Wang <250395@live.fr>
2019-11-23 13:26:01 +0100

    Added get_dummies Transformer, usable in Pipeline!

--- dataprep/feature_eng.py
+++ dataprep/feature_eng.py
@@ -2,43 +2,54 @@ import numpy as np
 import pandas as pd
 
 
+def drop_column(df, column):
+    """Return new DF without column"""
+    df2 = df.copy(deep=True)
+    df2 = df2.drop(columns=column)
+    return df2
+
+
 def bin_age(df):
-    """Returns df with new column 'age_bin' and without column 'age'"""
+    """Returns DF with new column 'age_bin'"""
+    df2 = df.copy(deep=True)
     bin_names = []
     for i in range(2, 8):
         bin_names.append(str(i) + '0s')
-    df['age_bin'] = pd.cut(df['age'], 6, labels=bin_names)
+    df2['age_bin'] = pd.cut(df2['age'], 6, labels=bin_names)
 
-    df['age_bin'] = df['age_bin'].cat.add_categories('60+')
-    df.loc[(df['age_bin'] == '60s') | (df['age_bin'] == '70s'), 'age_bin'] = '60+'
-    df['age_bin'] = df['age_bin'].cat.remove_unused_categories()
-    df = df.drop(columns='age')
-    return df
+    df2['age_bin'] = df2['age_bin'].cat.add_categories('60+')
+    df2.loc[(df2['age_bin'] == '60s') | (df2['age_bin'] == '70s'), 'age_bin'] = '60+'
+    df2['age_bin'] = df2['age_bin'].cat.remove_unused_categories()
+    return df2
 
 
 def gender_x_marriage(df):
-    """Returns df with a cross-categorical column gender x marriage"""
-    df['gen_mar'] = 0
-    df = df.assign(gen_mar=list((zip(df.gender, df.marriage))))
-    return df
+    """Returns DF with a cross-categorical column gender x marriage"""
+    df2 = df.copy(deep=True)
+    df2['gen_mar'] = 0
+    df2 = df2.assign(gen_mar=list((zip(df.gender, df.marriage))))
+    return df2
 
 
 def gender_x_agebin(df):
-    """Returns df with a cross-categorical column gender x ageBin"""
-    df['gen_ageBin'] = 0
-    df = df.assign(gen_ageBin=list(zip(df.gender, df.age_bin)))
-    return df
+    """Returns DF with a cross-categorical column gender x ageBin"""
+    df2 = df.copy(deep=True)
+    df2['gen_ageBin'] = 0
+    df2 = df2.assign(gen_ageBin=list(zip(df.gender, df.age_bin)))
+    return df2
 
 
 def next_bill_amt(df):
     """Returns df with prediction of next bill amount using exponential smoothing, to know if client will default"""
+    df2 = df.copy(deep=True)
+
     # Determining our best alpha
     alphas = np.linspace(0.9, 1, 100)
 
     median_errors = {}
 
     for alpha in alphas:
-        bill_amts_df = df[['bill_amt' + str(i) for i in range(6, 0, -1)]]
+        bill_amts_df = df2[['bill_amt' + str(i) for i in range(6, 0, -1)]]
         pondered_bill_amts_df = bill_amts_df.copy(deep=True)
         for i in range(6, 1, -1):
             pondered_bill_amts_df['bill_amt' + str(i)] = (1 - alpha) ** (i - 2) * pondered_bill_amts_df[
@@ -57,6 +68,6 @@ def next_bill_amt(df):
     for i in range(6, 0, -1):
         pondered_bill_amts_df['bill_amt' + str(i)] = (1 - alpha) ** (i - 1) * pondered_bill_amts_df['bill_amt' + str(i)]
     pondered_bill_amts_df['pred_bill_amt0'] = alpha * pondered_bill_amts_df.sum(axis=1)
-    df['pred_bill_amt0'] = pondered_bill_amts_df['pred_bill_amt0']
+    df2['pred_bill_amt0'] = pondered_bill_amts_df['pred_bill_amt0']
 
-    return df
+    return df2
--- dataprep/pipelines.py
+++ dataprep/pipelines.py
@@ -6,6 +6,19 @@ from sklearn.preprocessing import FunctionTransformer
 from .feature_eng import *
 
 
+# Column Dropper
+class ColumnDropper(BaseEstimator, TransformerMixin):
+
+    def __init__(self, column='age'):
+        self.column = column
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return drop_column(X, column=self.column)
+
+
 # Age Bin Adder
 class AgeBinAdder(BaseEstimator, TransformerMixin):
 
@@ -44,3 +57,30 @@ class NextBillAdder(BaseEstimator, TransformerMixin):
 
     def transform(self, X, y=None):
         return next_bill_amt(X)
+
+
+# https://masongallo.github.io/machine/learning,/python/2017/10/07/machine-learning-pipelines.html
+from pandas import Categorical, get_dummies
+from sklearn.base import TransformerMixin, BaseEstimator
+
+
+class CategoricalWarrior(BaseEstimator, TransformerMixin):
+    """One hot encoder for all categorical features"""
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
+    def fit(self, X, y=None):
+        cats = {}
+        for column in self.attribute_names:
+            cats[column] = X[column].unique().tolist()
+        self.categoricals = cats
+        return self
+
+    def transform(self, X, y=None):
+        df = X.copy()
+        for column in self.attribute_names:
+            df[column] = Categorical(df[column], categories=self.categoricals[column])
+        new_df = get_dummies(df)
+        # in case we need them later
+        self.columns = new_df.columns
+        return new_df
